bfc2ea84664726d4f00dab413253f49b934446d9
mstkpln <contactmstkpln@gmail.com>
2021-09-13 03:21:11 -0400

    first commit

---
+++ Final-Proje-Odevi-Mesut Kaplan.py
@@ -0,0 +1,45 @@
+__author__ = "Mesut Kaplan"
+while True:
+  print("Girdiğiniz rakama göre bulunduğunuz ay'ı size söyleyen programıma hoşgeldiniz")
+  secim = input("Lütfen sayıyı giriniz => sayi :")
+  isim = input("Lütfen adınızı yazınız :")
+  if secim =="1":
+    print("Ocak ayındasın", isim)
+    print
+  elif secim =="2":
+     print("Şubat ayındasın", isim)
+     print
+  elif secim == "3":
+      print("Mart ayındasın", isim)
+      print
+  elif secim == "4":
+      print("Nisan ayındasın", isim)
+      print
+  elif secim == "5":
+    print("Mayıs ayındasın", isim)
+    print
+  elif secim == "6":
+    print("Haziran ayındasın", isim)
+    print
+  elif secim == "7":
+    print("Temmuz ayındasın", isim)
+    print
+  elif secim == "8":
+    print("Ağustos ayındasın", isim)
+    print
+  elif secim == "9":
+    print("Eylül ayındasın", isim)
+    print
+  elif secim == "10":
+    print("Ekim ayındasın", isim)
+    print
+  elif secim == "11":
+    print("Kasım ayındasın", isim)
+    print
+  elif secim == "12":
+    print("Aralık ayındasın", isim)
+    print
+  else:
+    print ("Girilen ay rakamı hatalıdır. Program sonlandırılıyor...")
+
+
